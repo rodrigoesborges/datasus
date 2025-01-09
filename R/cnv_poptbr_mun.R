5d5096d4cf42785222df81f5e82ddf3703257268
Rodrigo Emmanuel Santana Borges <rodrigo@borges.net.br>
2025-01-09 15:45:09 -0300

    fix SXOption rvest for cnv_poptbr_mun

--- R/cnv_poptbr_mun.R
+++ R/cnv_poptbr_mun.R
@@ -47,7 +47,7 @@
 #' @importFrom utils head
 #' @export
 
-cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria 2", conteudo = 1, periodo = "last", regiao = "all",
+cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria 2", conteudo = 1, periodo = "last",
                            unidade_da_federacao = "all", municipio = "all",capital = "all", cir = "all", macrorregiao_de_saude = "all",
                            microrregiao_ibge = "all", ride = "all", territorio_da_cidadania = "all", mesorregiao_pndr = "all",
                            amazonia_legal = "all", semiarido = "all", faixa_de_fronteira = "all", zona_de_fronteira = "all",
@@ -72,61 +72,57 @@ cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria
 
   periodos.df <- data.frame(id = page %>% rvest::html_nodes("#A option") %>% rvest::html_text() %>% as.numeric(),
                             value = page %>% rvest::html_nodes("#A option") %>% rvest::html_attr("value"))
-  #adiciona regiao e uf cf tab e ref nac
-  regiao.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S1 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                           value = page %>% rvest::html_nodes("#S1 option") %>% rvest::html_attr("value")))
 
-  unidade_da_federacao.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S2 option") %>% rvest::html_text() %>% trimws(),
-                                                         value = page %>% rvest::html_nodes("#S2 option") %>% rvest::html_attr("value")))
+  unidade_da_federacao.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S1 option") %>% rvest::html_text() %>% trimws(),
+                                                         value = page %>% rvest::html_nodes("#S1 option") %>% rvest::html_attr("value")))
   unidade_da_federacao.df[] <- lapply(unidade_da_federacao.df, as.character)
 
-  municipios.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S3 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                               value = page %>% rvest::html_nodes("#S3 option") %>% rvest::html_attr("value")))
+  municipios.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S2 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                               value = page %>% rvest::html_nodes("#S2 option") %>% rvest::html_attr("value")))
 
-  capital.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S4 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                            value = page %>% rvest::html_nodes("#S4 option") %>% rvest::html_attr("value")))
+  capital.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S3 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                            value = page %>% rvest::html_nodes("#S3 option") %>% rvest::html_attr("value")))
 
-  cir.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S5 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                        value = page %>% rvest::html_nodes("#S5 option") %>% rvest::html_attr("value")))
+  cir.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S4 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                        value = page %>% rvest::html_nodes("#S4 option") %>% rvest::html_attr("value")))
 
-  macrorregiao_de_saude.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S6 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                                          value = page %>% rvest::html_nodes("#S6 option") %>% rvest::html_attr("value")))
+  macrorregiao_de_saude.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S5 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                                          value = page %>% rvest::html_nodes("#S5 option") %>% rvest::html_attr("value")))
 
-  #ajuste vários mudam a opção, sexo   16, para trás ajuste extrema_pobreza opção 15  etc
-  microrregiao_ibge.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S7 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                                      value = page %>% rvest::html_nodes("#S7 option") %>% rvest::html_attr("value")))
+  microrregiao_ibge.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S6 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                                      value = page %>% rvest::html_nodes("#S6 option") %>% rvest::html_attr("value")))
 
-  ride.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S8 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                         value = page %>% rvest::html_nodes("#S8 option") %>% rvest::html_attr("value")))
+  ride.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S7 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                         value = page %>% rvest::html_nodes("#S7 option") %>% rvest::html_attr("value")))
 
-  territorio_da_cidadania.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S9 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                                            value = page %>% rvest::html_nodes("#S9 option") %>% rvest::html_attr("value")))
+  territorio_da_cidadania.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S8 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                                            value = page %>% rvest::html_nodes("#S8 option") %>% rvest::html_attr("value")))
 
-  mesorregiao_pndr.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S10 option") %>% rvest::html_text() %>% readr::parse_number(),
-                                                     value = page %>% rvest::html_nodes("#S10 option") %>% rvest::html_attr("value")))
+  mesorregiao_pndr.df <- suppressWarnings(data.frame(id = page %>% rvest::html_nodes("#S9 option") %>% rvest::html_text() %>% readr::parse_number(),
+                                                     value = page %>% rvest::html_nodes("#S9 option") %>% rvest::html_attr("value")))
 
-  amazonia_legal.df <- data.frame(id = page %>% rvest::html_nodes("#S11 option") %>% rvest::html_text() %>% trimws(),
-                                  value = page %>% rvest::html_nodes("#S11 option") %>% rvest::html_attr("value"))
+  amazonia_legal.df <- data.frame(id = page %>% rvest::html_nodes("#S10 option") %>% rvest::html_text() %>% trimws(),
+                                  value = page %>% rvest::html_nodes("#S10 option") %>% rvest::html_attr("value"))
   amazonia_legal.df[] <- lapply(amazonia_legal.df, as.character)
 
-  semiarido.df <- data.frame(id = page %>% rvest::html_nodes("#S12 option") %>% rvest::html_text() %>% trimws(),
-                             value = page %>% rvest::html_nodes("#S12 option") %>% rvest::html_attr("value"))
+  semiarido.df <- data.frame(id = page %>% rvest::html_nodes("#S11 option") %>% rvest::html_text() %>% trimws(),
+                             value = page %>% rvest::html_nodes("#S11 option") %>% rvest::html_attr("value"))
   semiarido.df[] <- lapply(semiarido.df, as.character)
 
-  faixa_de_fronteira.df <- data.frame(id = page %>% rvest::html_nodes("#S13 option") %>% rvest::html_text() %>% trimws(),
-                                      value = page %>% rvest::html_nodes("#S13 option") %>% rvest::html_attr("value"))
+  faixa_de_fronteira.df <- data.frame(id = page %>% rvest::html_nodes("#S12 option") %>% rvest::html_text() %>% trimws(),
+                                      value = page %>% rvest::html_nodes("#S12 option") %>% rvest::html_attr("value"))
   faixa_de_fronteira.df[] <- lapply(faixa_de_fronteira.df, as.character)
 
-  zona_de_fronteira.df <- data.frame(id = page %>% rvest::html_nodes("#S14 option") %>% rvest::html_text() %>% trimws(),
-                                     value = page %>% rvest::html_nodes("#S14 option") %>% rvest::html_attr("value"))
+  zona_de_fronteira.df <- data.frame(id = page %>% rvest::html_nodes("#S13 option") %>% rvest::html_text() %>% trimws(),
+                                     value = page %>% rvest::html_nodes("#S13 option") %>% rvest::html_attr("value"))
   zona_de_fronteira.df[] <- lapply(zona_de_fronteira.df, as.character)
 
-  municipio_de_extrema_pobreza.df <- data.frame(id = page %>% rvest::html_nodes("#S15 option") %>% rvest::html_text() %>% trimws(),
-                                                value = page %>% rvest::html_nodes("#S15 option") %>% rvest::html_attr("value"))
+  municipio_de_extrema_pobreza.df <- data.frame(id = page %>% rvest::html_nodes("#S14 option") %>% rvest::html_text() %>% trimws(),
+                                                value = page %>% rvest::html_nodes("#S14 option") %>% rvest::html_attr("value"))
   municipio_de_extrema_pobreza.df[] <- lapply(municipio_de_extrema_pobreza.df, as.character)
 
 
-  municipios.df$id[1] <- regiao.df$id[1] <- unidade_da_federacao.df$id[1] <- capital.df$id[1] <- "all"
+  municipios.df$id[1] <- unidade_da_federacao.df$id[1] <- capital.df$id[1] <- "all"
   cir.df$id[1] <- macrorregiao_de_saude.df$id[1] <- microrregiao_ibge.df$id[1] <- "all"
   territorio_da_cidadania.df$id[1] <- mesorregiao_pndr.df$id[1] <- amazonia_legal.df$id[1] <- semiarido.df$id[1] <- "all"
   faixa_de_fronteira.df$id[1] <- zona_de_fronteira.df$id[1] <- municipio_de_extrema_pobreza.df$id[1] <- "all"
@@ -196,14 +192,7 @@ cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria
     if (!(all(periodo %in% periodos.df$id))) stop("The 'periodo' argument is misspecified")
 
   }
-  #adicionada região e UF
-  if (any(regiao != "all")) {
-
-    regiao <- as.character(regiao)
-
-    if (!(all(regiao %in% regiao.df$id))) stop("Some element in 'regiao' argument is wrong")
-
-  }
+  #adicionada  UF
 
   if (any(unidade_da_federacao != "all")) {
 
@@ -357,65 +346,66 @@ cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria
   form_periodo <- dplyr::filter(periodos.df, periodos.df$id %in% periodo)
   form_periodo <- paste0("Arquivos=", form_periodo$value, collapse = "&")
 
-  form_pesqmes1 <- "pesqmes1=Digite+o+texto+e+ache+f%E1cil"
 
-  #regiao
-  form_regiao <- dplyr::filter(regiao.df, regiao.df$id %in% regiao)
-  form_regiao <- paste0("SRegi%E3o=", form_regiao$value, collapse = "&")
-
-  form_pesqmes2 <- "pesqmes2=Digite+o+texto+e+ache+f%E1cil"
 
   #unidade_da_federacao
   form_unidade_da_federacao <- dplyr::filter(unidade_da_federacao.df, unidade_da_federacao.df$id %in% unidade_da_federacao)
   form_unidade_da_federacao <- paste0("SUnidade_da_Federa%E7%E3o=", form_unidade_da_federacao$value, collapse = "&")
 
-  form_pesqmes3 <- "pesqmes3=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes1 <- "pesqmes1=Digite+o+texto+e+ache+f%E1cil"
+
 
   #municipio
   form_municipio <- dplyr::filter(municipios.df, municipios.df$id %in% municipio)
   form_municipio <- paste0("SMunic%EDpio=", form_municipio$value, collapse = "&")
 
-  form_pesqmes4 <- "pesqmes4=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes2 <- "pesqmes2=Digite+o+texto+e+ache+f%E1cil"
+
 
   #capital
   form_capital <- dplyr::filter(capital.df, capital.df$id %in% capital)
   form_capital <- paste0("SCapital=", form_capital$value, collapse = "&")
 
-  form_pesqmes5 <- "pesqmes5=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes3 <- "pesqmes3=Digite+o+texto+e+ache+f%E1cil"
+
 
   #cir
   form_cir <- dplyr::filter(cir.df, cir.df$id %in% cir)
   form_cir <- paste0("SRegi%E3o_de_Sa%FAde_%28CIR%29=", form_cir$value, collapse = "&")
 
-  form_pesqmes6 <- "pesqmes6=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes4 <- "pesqmes4=Digite+o+texto+e+ache+f%E1cil"
 
   #macrorregiao_de_saude
   form_macrorregiao_de_saude <- dplyr::filter(macrorregiao_de_saude.df, macrorregiao_de_saude.df$id %in% macrorregiao_de_saude)
   form_macrorregiao_de_saude <- paste0("SMacrorregi%E3o_de_Sa%FAde=", form_macrorregiao_de_saude$value, collapse = "&")
 
-  form_pesqmes7 <- "pesqmes7=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes5 <- "pesqmes5=Digite+o+texto+e+ache+f%E1cil"
 
   #microrregiao_ibge
   form_microrregiao_ibge <- dplyr::filter(microrregiao_ibge.df, microrregiao_ibge.df$id %in% microrregiao_ibge)
   form_microrregiao_ibge <- paste0("SMicrorregi%E3o_IBGE=", form_microrregiao_ibge$value, collapse = "&")
 
-  form_pesqmes8 <- "pesqmes8=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes6 <- "pesqmes6=Digite+o+texto+e+ache+f%E1cil"
 
   #ride
   form_ride <- dplyr::filter(ride.df, ride.df$id %in% ride)
   form_ride <- paste0("SRegi%E3o_Metropolitana_-_RIDE=", form_ride$value, collapse = "&")
 
-  form_pesqmes9 <- "pesqmes9=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes7 <- "pesqmes7=Digite+o+texto+e+ache+f%E1cil"
+
 
   #territorio_da_cidadania
   form_territorio_da_cidadania <- dplyr::filter(territorio_da_cidadania.df, territorio_da_cidadania.df$id %in% territorio_da_cidadania)
   form_territorio_da_cidadania <- paste0("STerrit%F3rio_da_Cidadania=", form_territorio_da_cidadania$value, collapse = "&")
 
-  form_pesqmes10 <- "pesqmes10=Digite+o+texto+e+ache+f%E1cil"
+  form_pesqmes8 <- "pesqmes8=Digite+o+texto+e+ache+f%E1cil"
+
 
   #mesorregiao_pndr
   form_mesorregiao_pndr <- dplyr::filter(mesorregiao_pndr.df, mesorregiao_pndr.df$id %in% mesorregiao_pndr)
   form_mesorregiao_pndr <- paste0("SMesorregi%E3o_PNDR=", form_mesorregiao_pndr$value, collapse = "&")
+
+  form_pesqmes9 <- "pesqmes9=Digite+o+texto+e+ache+f%E1cil"
 
   #amazonia_legal
   form_amazonia_legal <- dplyr::filter(amazonia_legal.df, amazonia_legal.df$id %in% amazonia_legal)
@@ -437,11 +427,11 @@ cnv_poptbr_mun <- function(linha = "Munic\u00edpio", coluna = "Faixa Et\u00e1ria
   form_municipio_de_extrema_pobreza <- dplyr::filter(municipio_de_extrema_pobreza.df, municipio_de_extrema_pobreza.df$id %in% municipio_de_extrema_pobreza)
   form_municipio_de_extrema_pobreza <- paste0("SMunic%EDpio_de_extrema_pobreza=", form_municipio_de_extrema_pobreza$value, collapse = "&")
 
-  form_data <- paste(form_linha, form_coluna, form_conteudo, form_periodo, form_pesqmes1, form_regiao, form_pesqmes2,
+  form_data <- paste(form_linha, form_coluna, form_conteudo, form_periodo, form_pesqmes1, form_pesqmes2,
                      form_unidade_da_federacao, form_pesqmes3, form_municipio, form_pesqmes4,
                      form_capital, form_pesqmes5, form_cir, form_pesqmes6,form_macrorregiao_de_saude, form_pesqmes7,
                      form_microrregiao_ibge, form_pesqmes8, form_ride, form_pesqmes9, form_territorio_da_cidadania,
-                     form_pesqmes10, form_mesorregiao_pndr, form_amazonia_legal, form_semiarido,
+                     form_mesorregiao_pndr, form_amazonia_legal, form_semiarido,
                      form_faixa_de_fronteira, form_zona_de_fronteira, form_municipio_de_extrema_pobreza,
                      "formato=table&mostre=Mostra", sep = "&")
 
